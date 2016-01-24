73a97fec57288816478180d02e2cb10382bf8b1e
Aldar Antonov <aantonov84@gmail.com>
2016-01-24 05:17:00 +0300

    some changes

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,4 +1,5 @@
 class Racer
+  include ActiveModel::Model
   attr_accessor :id, :number, :first_name, :last_name, :gender, :group, :secs
 
   def initialize(params = {})
@@ -11,6 +12,48 @@ class Racer
     @secs = params[:secs].to_i
   end
 
+  def persisted?
+    !@id.nil?
+  end
+
+  def created_at
+    nil
+  end
+
+  def updated_at
+    nil
+  end
+
+  def destroy
+    self.class.collection.find(number: @number).delete_one
+  end
+
+  def update(params)
+    @number = params[:number].to_i
+    @first_name = params[:first_name]
+    @last_name = params[:last_name]
+    @gender = params[:gender]
+    @group = params[:group]
+    @secs = params[:secs].to_i
+    params.slice!(:number, :first_name, :last_name, :gender, :group, :secs) if ! params.nil?
+    self.class.collection.find(_id: BSON::ObjectId.from_string(@id)).replace_one(params)
+  end
+
+  def save
+    r = self.class.collection.insert_one(_id: @id,
+                                          number: @number,
+                                          first_name: @first_name,
+                                          last_name: @last_name,
+                                          gender: @gender,
+                                          group: @group,
+                                          secs: @secs)
+    @id = r.inserted_id.to_s
+  end
+
+  def self.find id
+    result = collection.find(_id: BSON::ObjectId.from_string(id.to_s) ).first
+    return result.nil? ? nil : Racer.new(result)
+  end
   def self.mongo_client
     Mongoid::Clients.default
   end
@@ -24,4 +67,19 @@ class Racer
     res = res.limit(limit) if ! limit.nil?
     return res
   end
+
+  def self.paginate(params)
+    page = (params[:page] || 1).to_i
+    limit = (params[:per_page] || 30).to_i
+    skip = (page - 1) * limit
+    racers = []
+    all({}, {}, skip, limit).each do |r|
+      racers << Racer.new(r)
+    end
+    total = all.count
+    WillPaginate::Collection.create(page, limit, total) do |pager|
+      pager.replace(racers)
+    end
+  end
+
 end
